5eabb1989bcb30c91fb07288536287e0e112c3c4
root <cardoso.nilo@gmail.com>
2016-01-19 10:06:08 -0200

    new test

--- projects/recwind_modifier/test/sim_recwind_read/run.py
+++ projects/recwind_modifier/test/sim_recwind_read/run.py
@@ -1,8 +1,8 @@
 #!/bin/env python
 
 from NFTest import *
-#from NFTest import simReg		# Not necessary
-#import time									# Not necessary
+#from CryptoNICLib import *
+#from reg_defines_crypto_nic import *
 
 phy2loop0 = ('../connections/conn', [])
 
@@ -27,45 +27,19 @@ i = 0
 port1 = 80
 port2 = 1010
 
-while i < 10:
-   hdr.dport=port1+i
-   hdr.sport=port2+i
-   hdr.flags = 0b00010
-   DA = "0xD0:0x27:0x88:0xBC:0xA8:0x%02x"%(i)
-   SA = "0x0:0x4E:0x46:0x32:0x43:0x%02x"%(i)
-   DST_IP = '192.168.101.%0.3i'%(i)
-   SRC_IP = '192.168.101.%0.3i'%(i+1)
-   pkt = scapy.Ether(dst=DA, src=SA)/scapy.IP(dst=DST_IP, 
-         src=SRC_IP, ttl=TTL)/hdr/load
-   pkt.len = (len(load))+eth_hdr+ipv4_hdr+tcp_hdr
-   seqn = i*(50)
-   pkt.seq = seqn 
-   i = i + 1
-   nftest_send_phy('nf2c0', pkt)
-   nftest_expect_dma('nf2c0', pkt)
-
-nftest_barrier()
-
-i=0
-
-while i < 10:
-   hdr.dport=port2+i
-   hdr.sport=port1+i
-   hdr.flags = 0b00010
-   DA = "0xD0:0x27:0x88:0xBC:0xA8:0x%02x"%(i)
-   SA = "0x0:0x4E:0x46:0x32:0x43:0x%02x"%(i)
-   DST_IP = '192.168.101.%0.3i'%(i)
-   SRC_IP = '192.168.101.%0.3i'%(i+1)
-   pkt = scapy.Ether(dst=DA, src=SA)/scapy.IP(dst=DST_IP, 
-         src=SRC_IP, ttl=TTL)/hdr/load
-   pkt.len = (len(load))+eth_hdr+ipv4_hdr+tcp_hdr
-   seqn = i*(50)
-   pkt.seq = seqn 
-   i = i + 1
-   nftest_send_phy('nf2c0', pkt)
-   #we only expect pkts not dropped by firewall.
-   #In simulation (only), we defined a static rule than all pkts
-   #with dport between 1010 and 1060 will be dropped
-
+hdr.dport=port1+i
+hdr.sport=port2+i
+hdr.flags = 0b00010
+DA = "0xD0:0x27:0x88:0xBC:0xA8:0x%02x"%(i)
+SA = "0x0:0x4E:0x46:0x32:0x43:0x%02x"%(i)
+DST_IP = '192.168.101.%0.3i'%(i)
+SRC_IP = '192.168.101.%0.3i'%(i+1)
+pkt = scapy.Ether(dst=DA, src=SA)/scapy.IP(dst=DST_IP, src=SRC_IP, ttl=TTL)/hdr/load
+pkt.len = (len(load))+eth_hdr+ipv4_hdr+tcp_hdr
+seqn = i*(50)
+pkt.seq = seqn 
+i = i + 1
+nftest_send_phy('nf2c0', pkt)
+nftest_expect_dma('nf2c0', pkt)
 
 nftest_finish()
